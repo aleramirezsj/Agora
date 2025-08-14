4cd64c2297315219d08bb5839b1d7f407e7a9a20
Rubén Alejandro Ramirez <aleramirezsj@gmail.com>
2025-08-14 18:57:30 -0300

    Agregar archivos de proyecto.

---
+++ Backend/DataContext/AgoraContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Service.Models;
+
+namespace Backend.DataContext
+{
+    public class AgoraContext: DbContext
+    {
+        public DbSet<TipoInscripcion> TipoInscripciones { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        public AgoraContext(){ }
+
+        public AgoraContext(DbContextOptions<AgoraContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Cargamos los datos iniciales de tipo de inscripciones(Público en general, Docentes, Estudiantes, Jubilados)
+            modelBuilder.Entity<TipoInscripcion>().HasData(
+                new TipoInscripcion { Id = 1, Nombre = "Público en general" },
+                new TipoInscripcion { Id = 2, Nombre = "Docentes" },
+                new TipoInscripcion { Id = 3, Nombre = "Estudiantes" },
+                new TipoInscripcion { Id = 4, Nombre = "Jubilados" }
+            );
+
+            //cargamos los datos iniciales de usuarios
+            modelBuilder.Entity<Usuario>().HasData(
+                new Usuario { Id = 1, Nombre = "Admin", Email = "admin@gmail.com", Password = "admin123", TipoUsuario = Service.Enums.TipoUsuarioEnum.Administrador },
+                new Usuario { Id = 2, Nombre = "Juan Perez", Email = "perez@gmail.com", Password = "juan123", TipoUsuario = Service.Enums.TipoUsuarioEnum.Estudiante });
+
+
+
+
+            // Configuramos las querys para que no devuelvan los elementos eliminados
+            modelBuilder.Entity<TipoInscripcion>().HasQueryFilter(p => !p.IsDeleted);
+            modelBuilder.Entity<Usuario>().HasQueryFilter(p => !p.IsDeleted);
+        }
+    }
+}
---
+++ Backend/Program.cs
@@ -0,0 +1,41 @@
+using Backend.DataContext;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var configuration = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+string? cadenaConexion = configuration.GetConnectionString("mysqlLocal");
+
+//configuración de inyección de dependencias del DBContext
+builder.Services.AddDbContext<AgoraContext>(
+    options => options.UseMySql(cadenaConexion,
+                                ServerVersion.AutoDetect(cadenaConexion),
+                    options => options.EnableRetryOnFailure(
+                                        maxRetryCount: 5,
+                                        maxRetryDelay: System.TimeSpan.FromSeconds(30),
+                                       errorNumbersToAdd: null)
+                                ));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
