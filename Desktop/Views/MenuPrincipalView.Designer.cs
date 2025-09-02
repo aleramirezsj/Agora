d551c508a864102e02722423d3178ea44f61219b
Rubén Alejandro Ramirez <aleramirezsj@gmail.com>
2025-09-02 17:02:56 -0300

    Añadir proyecto Desktop y vistas principales
    Se ha añadido un nuevo proyecto "Desktop" a la solución `Agora.sln`, incluyendo su configuración de compilación. Se han implementado las clases `MenuPrincipalView`, `SplashView` y `UsuariosView`, junto con sus archivos de diseño y recursos correspondientes. También se han realizado limpiezas en el código de `GenericService.cs` y se ha creado el archivo de proyecto `Desktop.csproj` con configuraciones específicas para aplicaciones de Windows.

---
+++ Desktop/Views/MenuPrincipalView.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Desktop
+{
+    partial class MenuPrincipalView
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuPrincipal = new MenuStrip();
+            SubMenuPrincipal = new FontAwesome.Sharp.IconMenuItem();
+            SubMenuSalir = new FontAwesome.Sharp.IconMenuItem();
+            SubMenuSalirDelSistema = new FontAwesome.Sharp.IconMenuItem();
+            toolStrip1 = new ToolStrip();
+            SubMenuUsuarios = new FontAwesome.Sharp.IconMenuItem();
+            menuPrincipal.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuPrincipal
+            // 
+            menuPrincipal.ImageScalingSize = new Size(24, 24);
+            menuPrincipal.Items.AddRange(new ToolStripItem[] { SubMenuPrincipal, SubMenuSalir });
+            menuPrincipal.Location = new Point(0, 0);
+            menuPrincipal.Name = "menuPrincipal";
+            menuPrincipal.Size = new Size(800, 33);
+            menuPrincipal.TabIndex = 1;
+            menuPrincipal.Text = "menuStrip1";
+            // 
+            // SubMenuPrincipal
+            // 
+            SubMenuPrincipal.DropDownItems.AddRange(new ToolStripItem[] { SubMenuUsuarios });
+            SubMenuPrincipal.IconChar = FontAwesome.Sharp.IconChar.HomeLg;
+            SubMenuPrincipal.IconColor = Color.Black;
+            SubMenuPrincipal.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            SubMenuPrincipal.Name = "SubMenuPrincipal";
+            SubMenuPrincipal.Size = new Size(118, 29);
+            SubMenuPrincipal.Text = "Principal";
+            // 
+            // SubMenuSalir
+            // 
+            SubMenuSalir.DropDownItems.AddRange(new ToolStripItem[] { SubMenuSalirDelSistema });
+            SubMenuSalir.IconChar = FontAwesome.Sharp.IconChar.DoorClosed;
+            SubMenuSalir.IconColor = Color.Black;
+            SubMenuSalir.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            SubMenuSalir.Name = "SubMenuSalir";
+            SubMenuSalir.Size = new Size(85, 29);
+            SubMenuSalir.Text = "Salir";
+            // 
+            // SubMenuSalirDelSistema
+            // 
+            SubMenuSalirDelSistema.IconChar = FontAwesome.Sharp.IconChar.DoorOpen;
+            SubMenuSalirDelSistema.IconColor = Color.Black;
+            SubMenuSalirDelSistema.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            SubMenuSalirDelSistema.Name = "SubMenuSalirDelSistema";
+            SubMenuSalirDelSistema.Size = new Size(270, 34);
+            SubMenuSalirDelSistema.Text = "Salir del sistema";
+            SubMenuSalirDelSistema.Click += SubMenuSalirDelSistema_Click;
+            // 
+            // toolStrip1
+            // 
+            toolStrip1.ImageScalingSize = new Size(24, 24);
+            toolStrip1.Location = new Point(0, 33);
+            toolStrip1.Name = "toolStrip1";
+            toolStrip1.Size = new Size(800, 25);
+            toolStrip1.TabIndex = 2;
+            toolStrip1.Text = "toolStrip1";
+            // 
+            // SubMenuUsuarios
+            // 
+            SubMenuUsuarios.IconChar = FontAwesome.Sharp.IconChar.UserFriends;
+            SubMenuUsuarios.IconColor = Color.Black;
+            SubMenuUsuarios.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            SubMenuUsuarios.Name = "SubMenuUsuarios";
+            SubMenuUsuarios.Size = new Size(270, 34);
+            SubMenuUsuarios.Text = "Usuarios";
+            SubMenuUsuarios.Click += SubMenuUsuarios_Click;
+            // 
+            // MenuPrincipalView
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(toolStrip1);
+            Controls.Add(menuPrincipal);
+            IsMdiContainer = true;
+            MainMenuStrip = menuPrincipal;
+            Name = "MenuPrincipalView";
+            Text = "Ágora : Software de Acreditación de Capacitaciones ISP20";
+            WindowState = FormWindowState.Maximized;
+            menuPrincipal.ResumeLayout(false);
+            menuPrincipal.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuPrincipal;
+        private ToolStrip toolStrip1;
+        private FontAwesome.Sharp.IconMenuItem SubMenuPrincipal;
+        private FontAwesome.Sharp.IconMenuItem SubMenuSalir;
+        private FontAwesome.Sharp.IconMenuItem SubMenuSalirDelSistema;
+        private FontAwesome.Sharp.IconMenuItem SubMenuUsuarios;
+    }
+}
---
+++ Desktop/Views/SplashView.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Desktop.Views
+{
+    partial class SplashView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // SplashView
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(23, 39, 55);
+            ClientSize = new Size(717, 300);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "SplashView";
+            Text = "SplashView";
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
