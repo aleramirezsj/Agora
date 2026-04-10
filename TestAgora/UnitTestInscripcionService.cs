80e8b0af106bf05b247a62aeda2975d8b4d2d09c
Arrieta Leonel <arrieta.o.leonel@gmail.com>
2026-04-10 10:02:54 -0300

    Añadir pruebas unitarias para InscripcionService
    Se ha creado la clase `UnitTestInscripcionService` con un método de prueba asíncrono `TestGetInscriptosAsync`. Esta prueba verifica que el método `GetInscriptosAsync` retorne un resultado válido, comprobando que no sea nulo, que sea del tipo `List<Inscripcion>` y que contenga elementos. También se realizan aserciones sobre las propiedades de cada inscripción y se imprime información relevante en la consola.

---
+++ TestAgora/UnitTestInscripcionService.cs
@@ -0,0 +1,42 @@
+﻿using Service.Models;
+using Service.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAgora
+{
+    public class UnitTestInscripcionService
+    {
+        [Fact]
+
+        public async void TestGetInscriptosAsync()
+        {
+            // Arrange
+            var service = new InscripcionService();
+
+            // Asumiendo que el ID 1 siempre tiene inscriptos en la base de datos
+            int id = 1;
+
+            // Act
+            var result = await service.GetInscriptosAsync(id);
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<List<Inscripcion>>(result);
+            Assert.True(result.Count > 0);
+
+            foreach (var item in result)
+            {
+                Assert.NotNull(item);
+                Assert.NotNull(item.Capacitacion);
+                Assert.NotNull(item.Usuario);
+                Assert.NotNull(item.TipoInscripcion);
+                Console.WriteLine($"Id: {item.Id}, Nombre Capacitación: {item.Capacitacion!.Nombre}, Nombre Usuario: {item.Usuario!.Nombre}, Tipo Inscripción: {item.TipoInscripcion!.Nombre}");
+            }
+        }
+
+    }
+}
+
